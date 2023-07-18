f1c14014d56012876775e07b840279c7dcc36777
Ashley Barrera <ashleyaee83@gmail.com>
2023-07-18 10:44:27 -0500

    Initial Commit

---
+++ buttonPage/buttonPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  buttonPageApp.swift
+//  buttonPage
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct buttonPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
